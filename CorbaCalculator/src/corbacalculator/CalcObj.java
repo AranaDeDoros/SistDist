08b97db0227e0290432fa209d977913a78fd829f
AranaDeDoros <davidfloresitver@gmail.com>
2018-03-16 13:56:51 -0600

    "corba cal"

---
+++ CorbaCalculator/src/corbacalculator/CalcObj.java
@@ -0,0 +1,33 @@
+package corbacalculator;
+
+/**
+ *
+ * @author imed
+ */
+import CalcApp.AdditionPOA;
+import org.omg.CosNaming.*;
+import org.omg.CosNaming.NamingContextPackage.*;
+import org.omg.CORBA.*;
+import org.omg.PortableServer.*;
+import org.omg.PortableServer.POA;
+import java.util.Properties;
+
+public class CalcObj extends AdditionPOA {
+
+    private ORB orb;
+
+    public void setORB(ORB orb_val) {
+        orb = orb_val;
+    }
+
+    // implement add() method
+    public int add(int a, int b) {
+        int r = a + b;
+        return r;
+    }
+
+    // implement shutdown() method
+    public void shutdown() {
+        orb.shutdown(false);
+    }
+}
