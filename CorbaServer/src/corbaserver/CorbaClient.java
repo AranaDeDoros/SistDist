dc50538a6fbe848f6131d3aba2117fbadf7b737b
AranaDeDoros <davidfloresitver@gmail.com>
2018-03-16 12:29:34 -0600

    minimal updates

---
+++ CorbaServer/src/corbaserver/CorbaClient.java
@@ -0,0 +1,39 @@
+package corbaserver;
+
+import HolaM.*;
+import corbaserver.*;
+import org.omg.CosNaming.*;
+import org.omg.CosNaming.NamingContextPackage.*;
+import org.omg.CORBA.*;
+
+public class CorbaClient {
+
+    static Hola holaImpl;
+
+    public static void main(String args[]) {
+        try {
+            // create and initialize the ORB
+            ORB orb = ORB.init(args, null);
+
+            // get the root naming context
+            org.omg.CORBA.Object objRef
+                    = orb.resolve_initial_references("NameService");
+            // Use NamingContextExt instead of NamingContext. This is 
+            // part of the Interoperable naming Service.  
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+
+            // resolve the Object Reference in Naming
+            String name = "Hello";
+            holaImpl = HolaHelper.narrow(ncRef.resolve_str(name));
+
+            System.out.println("Obtained a handle on server object: " + holaImpl);
+            System.out.println(holaImpl.diHola());
+            holaImpl.shutdown();
+
+        } catch (Exception e) {
+            System.out.println("ERROR : " + e);
+            e.printStackTrace(System.out);
+        }
+    }
+
+}
